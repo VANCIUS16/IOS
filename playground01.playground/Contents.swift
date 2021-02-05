5a0f7f48a319e279fe596b2adf203430f601a221
VANCIUS16 <67486850+VANCIUS16@users.noreply.github.com>
2021-02-04 22:29:34 -0800

    Batman
    Batman

---
+++ playground01.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var v1 = "esto es Swift"
+
+let c1 = 4
+
+var cadena:String = String ()
+
+cadena = "Hola Mundo" + v1
+print ("El contenido es: \(cadena)")
+print("El contenido es: \(c1)")
