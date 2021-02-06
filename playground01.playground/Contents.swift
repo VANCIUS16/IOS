25b16195ee64862c4251079608a96186064d36d0
Marc Nadal <al02876776@tecmilenio.mx>
2021-02-05 17:57:10 -0800

    Commit test :D

--- playground01.playground/Contents.swift
+++ playground01.playground/Contents.swift
@@ -1,12 +1,12 @@
 import UIKit
 
-var v1 = "esto es Swift"
+var v1 = "Esto es Swift"
 
 let c1 = 4
 
 var cadena:String = String ()
 
-cadena = "Hola Mundo" + v1
-print ("El contenido es: \(cadena)")
-print("El contenido es: \(c1)")
+cadena = "Hola Mundo, " + v1
+print ("El contenido de la cadena es: \(cadena)")
+print("El contenido de la variable es: \(c1)")
 print("FIN :D")
