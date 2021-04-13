00f591bf2bc714bef1c20e730960497d21eec62b
Marc Nadal <al02876776@tecmilenio.mx>
2021-04-12 21:46:22 -0700

    Actividad_10_MNP :D

---
+++ Actividad_10_MNP/Actividad_10_MNP/Actividad_10_MNP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad_10_MNP
+//
+//  Created by user190573 on 4/12/21.
+//  Copyright © 2021 user190573. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
