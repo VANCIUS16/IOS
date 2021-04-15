160f364e0dba4ee45cb1651cf4d9e9d8e39d647a
Marc Nadal <al02876776@tecmilenio.mx>
2021-04-14 22:22:34 -0700

    Avance 2 de la evidencia de IOS MNP

---
+++ Avance_2_evidencia_MNP/Avance_2_evidencia_MNP/Avance_2_evidencia_MNP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avance_2_evidencia_MNP
+//
+//  Created by user190573 on 4/14/21.
+//  Copyright © 2021 user190573. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
