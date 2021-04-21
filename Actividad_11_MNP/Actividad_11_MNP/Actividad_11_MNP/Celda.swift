4b4bfdb092111c0cace642f466031fe91d9147f1
Marc Nadal <al02876776@tecmilenio.mx>
2021-04-20 18:31:13 -0700

    Primer commit par evitar que se borre el proyecto para subir fotos F

---
+++ Actividad_11_MNP/Actividad_11_MNP/Actividad_11_MNP/Celda.swift
@@ -0,0 +1,13 @@
+//
+//  Celda.swift
+//  Actividad_11_MNP
+//
+//  Created by user190573 on 4/20/21.
+//  Copyright © 2021 user190573. All rights reserved.
+//
+
+import UIKit
+
+class Celda: UICollectionViewCell {
+     @IBOutlet weak var imagenNum: UIImageView!
+}
---
+++ Actividad_11_MNP/Actividad_11_MNP/Actividad_11_MNP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad_11_MNP
+//
+//  Created by user190573 on 4/20/21.
+//  Copyright © 2021 user190573. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
