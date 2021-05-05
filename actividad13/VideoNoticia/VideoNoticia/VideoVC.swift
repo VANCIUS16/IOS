419a2b12ac5893351ed5c9adab7d1eb5d3c64c40
VANCIUS16 <67486850+VANCIUS16@users.noreply.github.com>
2021-05-04 21:44:51 -0700

    Add files via upload
    Subida de documento para realizar la actividad 13

---
+++ actividad13/VideoNoticia/VideoNoticia/VideoVC.swift
@@ -0,0 +1,30 @@
+//
+//  VideoVC.swift
+//  VideoNoticia
+//
+//  Created by alicharlie on 12/05/16.
+//  Copyright © 2016 codepix. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+
+class VideoVC: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let ligaVideo = "http://walterebert.com/playground/video/hls/sintel-trailer.m3u8"
+    
+        
+        
+        
+        
+    }
+
+    
+    
+
+   }
