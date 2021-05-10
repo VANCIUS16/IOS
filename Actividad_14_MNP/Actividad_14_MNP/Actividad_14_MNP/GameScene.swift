b8530f505f8861fa110443a339e1b27fb48db9df
Marc Nadal <al02876776@tecmilenio.mx>
2021-05-10 16:28:40 -0700

    NAIS

--- Actividad_14_MNP/Actividad_14_MNP/Actividad_14_MNP/GameScene.swift
+++ Actividad_14_MNP/Actividad_14_MNP/Actividad_14_MNP/GameScene.swift
@@ -16,21 +16,22 @@ class GameScene: SKScene
     override func didMove(to view: SKView)
     {
         fondo.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-        num1.position = CGPoint(x: 0, y: -200)
+        num1.position = CGPoint(x: 0, y: -3600)
+        num1.size = CGSize(width: 100, height: 100)
         fondo.zPosition = -1
-        addChild(fondo)
         
+        addChild(fondo)
         fondo.addChild(num1)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-        num1.run(SKAction.move(to: CGPoint(x: 0, y: fondo.size.height), duration: 5))
+        num1.run(SKAction.move(to: CGPoint(x: 0, y: fondo.size.height), duration: 0.05))
         self.num1.position = CGPoint.zero
     }
    
     override func update(_ currentTime: CFTimeInterval)
     {
-        num1.position = CGPoint(x: 0, y: -200)
+        num1.position = CGPoint(x: 0, y: -360)
     }
 }
